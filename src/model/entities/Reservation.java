86a6ebf9f880f6b3798b31a12e279130a71336ed
Renato <renatoetali@gmail.com>
2019-09-05 16:03:38 -0300

    solução muito ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -55,7 +55,7 @@ public class Reservation {
 			   + sdf.format(checkin)
 			   + ", check-out: "
 			   + sdf.format(checkout)
-			   + ", "
+			   + ", "    
 			   + duration()
 			   + " nights";
 	}
